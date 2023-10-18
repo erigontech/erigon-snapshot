776b4a23e7245519ba08869421da303e418497b5
alex.sharov <AskAlexSharov@gmail.com>
2023-10-18 11:14:05 +0700

    save

---
+++ webseed/embed.go
@@ -0,0 +1,29 @@
+package snapshothashes
+
+import (
+	_ "embed"
+)
+
+//go:embed mainnet.toml
+var Mainnet []byte
+
+//go:embed goerli.toml
+var Goerli []byte
+
+//go:embed ropsten.toml
+var Ropsten []byte
+
+//go:embed sepolia.toml
+var Sepolia []byte
+
+//go:embed mumbai.toml
+var Mumbai []byte
+
+//go:embed bor-mainnet.toml
+var BorMainnet []byte
+
+//go:embed gnosis.toml
+var Gnosis []byte
+
+//go:embed chiado.toml
+var Chiado []byte
