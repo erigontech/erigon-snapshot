f64407a77e8e0c3a84d51ff3f9f2ee4d654bb252
awskii <awskii@users.noreply.github.com>
2024-10-23 03:42:58 +0100

    Check status code on fetching snapshot hashes (#321)
    ---------
    
    Co-authored-by: alex.sharov <AskAlexSharov@gmail.com>

--- embed.go
+++ embed.go
@@ -1,6 +1,7 @@
 package snapshothashes
 
 import (
+	"context"
 	_ "embed"
 	"fmt"
 	"io"
@@ -45,7 +46,7 @@ func getURLByChain(chain string) string {
 	return fmt.Sprintf("https://raw.githubusercontent.com/erigontech/erigon-snapshot/%s/%s.toml", branchReference, chain)
 }
 
-func LoadSnapshots() (couldFetch bool) {
+func LoadSnapshots(ctx context.Context) (fetched bool, err error) {
 	var (
 		mainnetUrl    = getURLByChain("mainnet")
 		sepoliaUrl    = getURLByChain("sepolia")
@@ -56,60 +57,66 @@ func LoadSnapshots() (couldFetch bool) {
 		holeskyUrl    = getURLByChain("holesky")
 	)
 	var hashes []byte
-	var err error
 	// Try to fetch the latest snapshot hashes from the web
-	if hashes, err = fetchSnapshotHashes(mainnetUrl); err != nil {
-		couldFetch = false
+	if hashes, err = fetchSnapshotHashes(ctx, mainnetUrl); err != nil {
+		fetched = false
 		return
 	}
 	Mainnet = hashes
 
-	if hashes, err = fetchSnapshotHashes(sepoliaUrl); err != nil {
-		couldFetch = false
+	if hashes, err = fetchSnapshotHashes(ctx, sepoliaUrl); err != nil {
+		fetched = false
 		return
 	}
 	Sepolia = hashes
 
-	if hashes, err = fetchSnapshotHashes(amoyUrl); err != nil {
-		couldFetch = false
+	if hashes, err = fetchSnapshotHashes(ctx, amoyUrl); err != nil {
+		fetched = false
 		return
 	}
 	Amoy = hashes
 
-	if hashes, err = fetchSnapshotHashes(borMainnetUrl); err != nil {
-		couldFetch = false
+	if hashes, err = fetchSnapshotHashes(ctx, borMainnetUrl); err != nil {
+		fetched = false
 		return
 	}
 	BorMainnet = hashes
 
-	if hashes, err = fetchSnapshotHashes(gnosisUrl); err != nil {
-		couldFetch = false
+	if hashes, err = fetchSnapshotHashes(ctx, gnosisUrl); err != nil {
+		fetched = false
 		return
 	}
 	Gnosis = hashes
 
-	if hashes, err = fetchSnapshotHashes(chiadoUrl); err != nil {
-		couldFetch = false
+	if hashes, err = fetchSnapshotHashes(ctx, chiadoUrl); err != nil {
+		fetched = false
 		return
 	}
 	Chiado = hashes
 
-	if hashes, err = fetchSnapshotHashes(holeskyUrl); err != nil {
-		couldFetch = false
+	if hashes, err = fetchSnapshotHashes(ctx, holeskyUrl); err != nil {
+		fetched = false
 		return
 	}
 	Holesky = hashes
 
-	couldFetch = true
-	return
+	fetched = true
+	return fetched, nil
 }
 
-func fetchSnapshotHashes(url string) ([]byte, error) {
-	resp, err := http.Get(url)
+func fetchSnapshotHashes(ctx context.Context, url string) ([]byte, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch snapshot hashes by %q: status code %d %s", url, resp.StatusCode, resp.Status)
+	}
 	res, err := io.ReadAll(resp.Body)
 	if len(res) == 0 {
 		return nil, fmt.Errorf("empty response from %s", url)
