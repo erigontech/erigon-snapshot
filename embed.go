d932b5731dad7b55f673129d6f6af0d8a59ab4d3
Willian Mitsuda <wmitsuda@gmail.com>
2026-05-07 21:16:53 -0300

    Mark SnapshotSource and source constants as kept for erigon 3.4
    Document why these symbols stayed behind: erigon 3.4 still imports
    them via FetchChainToml. They should be removed only after erigon/main
    defines its own copies and stops referencing this module's versions.

--- embed.go
+++ embed.go
@@ -25,8 +25,10 @@ var ArbSepolia []byte
 //go:embed bloatnet.toml
 var Bloatnet []byte
 
+// TODO: this type is used by erigon 3.4, DO NOT REMOVE IT YET; we should internalize it in erigon/main and remove it from here on erigon-snapshot/main
 type SnapshotSource int
 
+// TODO: these constants are used by erigon 3.4, DO NOT REMOVE IT YET; we should internalize them in erigon/main and remove them from here on erigon-snapshot/main
 const (
 	Github SnapshotSource = 0
 	R2     SnapshotSource = 1
